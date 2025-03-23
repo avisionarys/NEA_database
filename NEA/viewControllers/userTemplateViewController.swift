cf7d2a8f90911fcaf2672e0b446a0ac09a85f306
avisionarys <18avisrolia@salesian.surrey.sch.uk>
2025-03-23 12:51:34 +0000

    creating functionality for templates

--- NEA/viewControllers/userTemplateViewController.swift
+++ NEA/viewControllers/userTemplateViewController.swift
@@ -21,6 +21,7 @@ class userTemplateViewController: UIViewController {
     
     let templateVC = templateViewController()
 
+    @IBOutlet weak var usertemplateTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -62,7 +63,66 @@ class userTemplateViewController: UIViewController {
             }
         }
     }
+    
+    //create the array full of workoutdata objects
+    var dataForExercise: [WorkoutData] = []
+    // using dispatchGroup to synchronise tasks
+    let dispatchGroup = DispatchGroup()
+    // pull and save the data to the databas
+    @IBAction func saveWorkout(_ sender: Any) {
+        for i in 0..<usertemplateTable.numberOfRows(inSection: 0) {
+            guard let cell = usertemplateTable.cellForRow(at: IndexPath(row: i, section: 0)) as? TemplateTableViewCell else { continue }
+            let exerciseName = cell.nameOfExercise.text ?? ""
+            let weight = cell.weightTextField.text ?? ""
+            let reps = cell.repsTextField.text ?? ""
+            
+            //checks if any textfields are empty first
+            if exerciseName.isEmpty || weight.isEmpty || reps.isEmpty {
+                print("One or more text fields are empty. Please fill them out.")
+                return
+            }
+            var muscleGroup:String? = nil
+            // starts the synchronization of getting the muscleGroup and creating workout objects
+            dispatchGroup.enter()
+            
+            templateVC.getMuscleForExercise(exerciseName: exerciseName) { muscle in
+                if let muscle = muscle {
+                    muscleGroup = muscle
+                } else {
+                    print("Could not retrieve muscle information.")
+                }
+                
+                // Create the workout data object
+                let workoutData = WorkoutData(exerciseName: exerciseName, weight: weight, reps: reps, muscleGroup: muscleGroup ?? "nil")
+                self.dataForExercise.append(workoutData)
+                
+                // Leave the dispatch group after it has completed
+                self.dispatchGroup.leave()
+            }
+        }
+        //this will run after all the tasks above have finished
+        dispatchGroup.notify(queue: .main) {
+            do {
+                // Attempt to save to Firebase
+                try self.templateVC.saveToFirebase(data: self.dataForExercise)
 
+                // Navigate to the home view controller if successful
+                if let viewControllers = self.navigationController?.viewControllers {
+                    for viewController in viewControllers {
+                        if viewController is homeViewController {
+                            self.navigationController?.popToViewController(viewController, animated: true)
+                            return
+                        }
+                    }
+                }
+            } catch {
+                // Handle the error appropriately
+                print("Error saving data: \(error.localizedDescription)")
+            }
+        }
+        
+        
+    }
     
 
 
@@ -80,6 +140,38 @@ extension userTemplateViewController: UITableViewDataSource {//datasource tells 
         
         let exerciseName = exerciseNames[indexPath.row]
         cell.nameOfExercise.text = exerciseName
+        
+        let exerciseNameLabel = cell.nameOfExercise.text ?? "No text"
+        //calls the function and displays the personal record for user
+        templateVC.findMaxExerciseWeight(for: exerciseNameLabel) { result in
+            switch result {
+            case .success(let maxWeight):
+                print("Max \(exerciseNameLabel) weight: \(maxWeight)")
+                let weightLabel = "\(maxWeight)"
+                cell.recordWeight.text = weightLabel
+            case .failure(let error):
+                print("Error fetching max weight: \(error)")
+            }
+        }
+        //calls the function from the TemplateTableViewController and pulls the previous values
+        templateVC.findMostRecentExerciseData(for: exerciseNameLabel) { result in
+            switch result {
+            case .success(let data):
+                // Store weight and reps as constants
+                let mostRecentWeight = data.weight
+                let mostRecentReps = data.reps
+                print("Most recent weight for \(exerciseNameLabel): \(mostRecentWeight)")
+                print("Most recent reps for \(exerciseNameLabel): \(mostRecentReps)")
+                // sets the labels in the tableView to theeir specific values
+                cell.previousWeight.text = mostRecentWeight
+                cell.previousReps.text = mostRecentReps
+
+            case .failure(let error):
+                // Handles the error
+                print("Error fetching most recent exercise data: \(error.localizedDescription)")
+            }
+        }
+        
         return cell
     }
 }
